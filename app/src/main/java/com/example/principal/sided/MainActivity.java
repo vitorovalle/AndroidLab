5562baf02aa1724a5c3e21b763c00e9e9fac571a
vitorovalle <Saturnia19>
2016-10-18 23:12:10 -0200

    P1

--- app/src/main/java/com/example/principal/sided/MainActivity.java
+++ app/src/main/java/com/example/principal/sided/MainActivity.java
@@ -4,25 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
+import static com.example.principal.sided.R.id.BotaoAcessar;
+import static com.example.principal.sided.R.id.activity_main;
+import static com.example.principal.sided.R.id.activity_segunda_tela;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoAcessar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        botaoAcessar = (Button) findViewById(R.id.BotaoAcessar);
 
-    BotaoAcessar.setOnClickListener(new OnClickListener() {
-        @Override
-        public void onClick(View v) {
+        botaoAcessar.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            Intent trocadorTela = new Intent(MainActivity.this, SegundaTelaActivity.class);
-            startActivity(trocadorTela);
+                Intent trocadorTela = new Intent(MainActivity.this, SegundaTelaActivity.class);
+                startActivity(trocadorTela);
 
-        }
+            }
+        });
+
+
     }
 
 
--- app/src/main/java/com/example/principal/sided/SegundaTelaActivity.java
+++ app/src/main/java/com/example/principal/sided/SegundaTelaActivity.java
@@ -2,12 +2,31 @@ package com.example.principal.sided;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.principal.sided.R.id.BotaoSair;
+import static com.example.principal.sided.R.id.activity_main;
+import static com.example.principal.sided.R.id.activity_segunda_tela;
 
 public class SegundaTelaActivity extends AppCompatActivity {
+
+    private Button botaoVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_tela);
+
+        botaoVoltar = (Button) findViewById(R.id.BotaoSair);
+
+
+    botaoVoltar.setOnClickListener(new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+            SegundaTelaActivity.this.finish();
+        }
+    });
     }
-}
+    }
