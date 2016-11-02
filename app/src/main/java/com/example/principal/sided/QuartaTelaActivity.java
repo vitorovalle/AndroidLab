024cc0907ec43ef4928f704e03fba5a5cafe8058
Bruno C. Penha <bruno.penha@uniriotec.br>
2016-11-02 11:40:22 -0200

    Revisao no trabalho

--- app/src/main/java/com/example/principal/sided/QuartaTelaActivity.java
+++ app/src/main/java/com/example/principal/sided/QuartaTelaActivity.java
@@ -1,7 +1,7 @@
 package com.example.principal.sided;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class QuartaTelaActivity extends AppCompatActivity {
 
